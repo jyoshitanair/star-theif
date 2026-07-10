278ac777695672f189538c77b0a2e478288b6059
jyoshitanair <jyoshitanair123@gmail.com>
2026-07-09 18:59:31 -0700

    added cards working on sliding animatino and added a main menu

--- scripts/card.gd
+++ scripts/card.gd
@@ -2,17 +2,40 @@ extends Node2D
 @export var color:Color = Color("#95abdc")
 @export var text:String = "googesjhljhkjlhjhjhjhljkhjhkjlhjhjkhlhjhlkhjhkjhlhjhl"
 @export var texture: Texture2D = preload("res://icon.svg")
-@onready var color_rect: ColorRect = $ColorRect
-@onready var color_rect_2: ColorRect = $ColorRect2
+@onready var color_rect: Panel = $ColorRect
+@onready var color_rect_2: Panel = $ColorRect2
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var label: Label = $Label
-
+@onready var panel: Panel = $Panel
+@onready var panel_2: Panel = $Panel2
+var current = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	color_rect.color = color
-	color_rect_2.color = color.lightened(0.4)
 	sprite_2d.texture = texture
 	label.text = text
+	##
+	var style = panel.get_theme_stylebox("panel").duplicate()
+	style.bg_color = color.lightened(0.4)
+	panel.add_theme_stylebox_override("panel", style)
+	##
+	var style2 = color_rect_2.get_theme_stylebox("panel").duplicate()
+	style2.bg_color = color.darkened(0.4)
+	color_rect.add_theme_stylebox_override("panel", style2)
+	##
+	var style3 = color_rect_2.get_theme_stylebox("panel").duplicate()
+	style3.bg_color = color
+	color_rect_2.add_theme_stylebox_override("panel", style3)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if current:
+		panel.show()
+		panel_2.hide()
+	else:
+		panel_2.show()
+		panel.hide()
+	print(current)
+
+func _on_area_2d_mouse_entered() -> void:
+	current = true
+func _on_area_2d_mouse_exited() -> void:
+	current = false
---
+++ scripts/fightbar.gd
@@ -0,0 +1,49 @@
+extends Node2D
+var card = preload("res://scenes/card.tscn")
+var first1 = true
+var first2 = true
+var first3 = true
+var first4 = true
+var first5 = true
+############
+var loaded_card0
+var loaded_card_end0
+var loaded_card_end1
+var loaded_card_end2
+var loaded_card_end3
+var loaded_card_end4
+var loaded_card_end5
+var loaded_card1
+var loaded_card2
+var loaded_card3
+var loaded_card4
+var positions_array = [Vector2(120.0,774.0), Vector2(346.0,774.0),Vector2(571.0,774.0),Vector2(798.0,774.0),Vector2(1029.0,774.0)]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in range(0,5):
+		var card_name ="loaded_card%d"%i
+		var card_name_end ="loaded_card_end%d"%i
+		var temp_card = card.instantiate()
+		temp_card.position = positions_array[i]
+		add_child(temp_card)
+		set(card_name, temp_card)
+		set(card_name_end, (temp_card.position.y -198))
+		
+	await get_tree().create_timer(0.8).timeout
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if first1 ||first2 ||first3 ||first4 ||first5:
+		for i in range(0,5):
+			var card_name1 ="loaded_card%d"%i
+			var card_name_end1 ="loaded_card_end%d"%i
+			var actual_card1  = get(card_name1)
+			if actual_card1:
+				var target_pos = lerp(actual_card1.position.y,get(card_name_end1), delta*10 )
+				#sets a certain property
+				actual_card1.set_indexed("position:y",target_pos)
+				if is_equal_approx(get(card_name_end1),target_pos):
+					var firstvariation = "first%d"%(i+1)
+					set(firstvariation, false)
+			break
+				
